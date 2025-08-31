d363cc2c396635435a79eaf71f09819e03e24af5
Rizal <rizalsofiana1976@gmail.com>
2025-08-31 22:20:36 +0700

    menggunakan use effect pada fetch data product

---
+++ src/product/ProductList.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useRef, useState } from "react";
+import Product from "./Product";
+
+export default function ProductList() {
+    const [products, setProduct] = useState([])
+    const [load, setLoad] = useState(false)
+
+    function handleClick() {
+        setLoad(true)
+    }
+
+    useEffect(() => {
+        console.info("use effect with []")
+    }, [])
+
+    useEffect(() => {
+        console.info("Call use effect")
+
+        async function fetchProducts() {
+            const response = await fetch("/public/products.json")
+            const data = await response.json()
+            setProduct(data)
+        }
+
+        if (load) {
+            fetchProducts()
+        }
+
+        return () => {
+            console.log("Product list component unmounted");
+        }
+    }, [load])
+
+    return (
+        <>
+            <h1>Product List</h1>
+            <button onClick={handleClick}>Load Products</button>
+            {products.map((product) => (
+                <Product product={product} key={product.id} />
+            ))}
+        </>
+    )
+}

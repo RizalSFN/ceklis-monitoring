843d2d5fadb077b7b06e2e0fcf66b0ce6828d52b
Rizal <rizalsofiana1976@gmail.com>
2025-09-04 23:39:30 +0700

    registrasi route login

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 import express from "express"
 import userRouter from "./routes/user.routes.js"
+import authRouter from "./routes/auth.routes.js"
 
 const app = express()
 app.use(express.json())
 
 app.use("/api/users", userRouter)
+app.use("/api/auth", authRouter)
 
 app.listen(5000, () => {
     console.log("Server running on http://localhost:5000");

037545750545026fc4ef40c776b7b7ac644378ee
Rizal <rizalsofiana1976@gmail.com>
2025-09-17 22:22:05 +0700

    registrasi routes area ke main routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,14 @@
 import express from "express"
 import userRouter from "./routes/user.routes.js"
 import authRouter from "./routes/auth.routes.js"
+import areaRouter from "./routes/area.routes.js"
 
 const app = express()
 app.use(express.json())
 
 app.use("/api/users", userRouter)
 app.use("/api/auth", authRouter)
+app.use("/api/area", areaRouter)
 
 app.listen(5000, () => {
     console.log("Server running on http://localhost:5000");

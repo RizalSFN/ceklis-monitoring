84fe28219238d831fcca0508a598bc8ac7204154
Rizal <rizalsofiana1976@gmail.com>
2025-09-25 22:34:44 +0700

    integrasi checklist session routes ke main routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import userRouter from "./routes/user.routes.js"
 import authRouter from "./routes/auth.routes.js"
 import areaRouter from "./routes/area.routes.js"
 import workScheduleRouter from "./routes/workSchedule.routes.js"
+import checklistSessionRouter from "./routes/checklistSession.routes.js"
 
 const app = express()
 app.use(express.json())
@@ -11,6 +12,7 @@ app.use("/api/users", userRouter)
 app.use("/api/auth", authRouter)
 app.use("/api/area", areaRouter)
 app.use("/api/work-schedule", workScheduleRouter)
+app.use("/api/checklist-session", checklistSessionRouter)
 
 app.listen(5000, () => {
     console.log("Server running on http://localhost:5000");

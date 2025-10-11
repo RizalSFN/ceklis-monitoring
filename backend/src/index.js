16e344c6ce1409c43520f772ae31e6403d0c16c7
Rizal <rizalsofiana1976@gmail.com>
2025-10-11 18:15:29 +0700

    use task route di main route

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,7 @@ import areaRouter from "./routes/area.routes.js"
 import workScheduleRouter from "./routes/workSchedule.routes.js"
 import checklistSessionRouter from "./routes/checklistSession.routes.js"
 import checklistResultRouter from "./routes/checklistResult.routes.js"
+import taskRouter from "./routes/task.routes.js"
 
 const app = express()
 
@@ -20,6 +21,7 @@ app.use(express.json())
 app.use("/api/users", userRouter)
 app.use("/api/auth", authRouter)
 app.use("/api/area", areaRouter)
+app.use("/api/task", taskRouter)
 app.use("/api/work-schedule", workScheduleRouter)
 app.use("/api/checklist-session", checklistSessionRouter)
 app.use("/api/checklist-result", checklistResultRouter)

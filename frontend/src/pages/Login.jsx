ebca2bd857d2de6dc405ac7262bbdd51ce5d5237
Rizal <rizalsofiana1976@gmail.com>
2025-10-14 22:45:19 +0700

    update copyright

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -106,7 +106,7 @@ export default function Login() {
                 </form>
 
                 <p className="mt-10 text-center text-sm/6 text-gray-500">
-                    Copyright@
+                    Copyright@BagiKopiSoreang
                     <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
                         2025
                     </a>
